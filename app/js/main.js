054242a960f4214ce7255f262ab6478e68a12201
Lovely.Value <aid.l.v.esd@gmail.com>
2023-09-27 12:17:50 +0300

    Lesson two

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,23 @@
-console.log(1);
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+
+console.log(name);
+var name ='Ivan';
+
+{
+    let result = 50;
+}
