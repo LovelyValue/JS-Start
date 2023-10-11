ccde180990cfdc0c092d4293896da72e6de6da6f
Lovely.Value <aid.l.v.esd@gmail.com>
2023-10-11 11:51:35 +0300

    Lesson 17

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,31 @@
-let num = 50;
-
-// while (num <= 55) {
-// 	console.log(num);
-// 	num++;
-// }
-
-// do {
-// 	console.log(num);
-// 	num++
-// }
-// while (num < 55);
-
-// for (let i = 1; i < 8; i++) {
+// for (let i = 0; i < 3; i++) {
 // 	console.log(i);
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log(j);
+// 	}
 // }
 
-for (let i = 1; i < 8; i++) {
-	if (i === 6) {
-		// break;
-		continue;
+// let result = '';
+// const length = 7;
+
+// for (let i = 1; i < length; i++) {
+
+// 	for (let j = 0; j < i; j++) {
+// 		result += "*";
+// 	}
+
+// 	result += '\n';
+// }
+
+// console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+	console.log(`First level: ${i}`);
+	for (let j = 0; j < 3; j++) {
+		console.log(`Second level: ${j}`);
+		for (let k = 0; k < 3; k++) {
+			if (k === 2) continue first;
+			console.log(`Third level: ${k}`);
+		}
 	}
-	console.log(i);
 }
