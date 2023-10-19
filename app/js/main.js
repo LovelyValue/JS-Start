b945fc216204aceda71fdb5b1a0881149c5e7db2
Lovely.Value <aid.l.v.esd@gmail.com>
2023-10-19 14:12:39 +0300

    Lesson 27

--- app/js/main.js
+++ app/js/main.js
@@ -1,42 +1,27 @@
-const options = {
-	name: 'test',
-	width: 1024,
-	height: 1024,
-	colors: {
-		border: 'black',
-		bg: 'red'
-	},
-	makeTest: function() {
-		console.log('Test');
-	}
-};
+const arr = [1, 2, 3, 6, 8];
 
-options.makeTest();
+arr.pop();//удаление последнего элемента
+arr.push(10);//добавление элемента в конец
+arr.sort(compareNum);
+console.log(arr);
 
-const {border, bg} = options.colors;
+function compareNum(a, b) {
+	return a - b;
+}
 
-console.log(border);
+arr.forEach(function(item, i, arr) {
+	console.log(`${i}: ${item} inside massiv ${arr}`)
+});
 
-// console.log(Object.keys(options).length);
+for (let i = 0; i < arr.length; i++) {
+	console.log(arr[i]);
+}
 
-// console.log(options.name);
+for (let value of arr) {
+	console.log(value);
+}
 
-delete options.name;
-
-// console.log(options);
-
-// let counter = 0;
-// for (let key in options) {
-// 	if (typeof(options[key]) === 'object') {
-// 		for (let i in options[key]) {
-// 			console.log(`Свойства ${i}, имеет значение ${options[key] [i]}`);
-// 			counter++;
-// 		}
-// 	} else {
-// 		console.log(`Свойства ${key}, имеет значение ${options[key]}`);
-// 		counter++;
-// 	}
-// }
-
-// console.log(counter);
-
+const str = prompt('', '');
+const products = str.split(', ');
+products.sort();
+console.log(products.join('; '));
