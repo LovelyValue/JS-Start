898441bc4fbe9e3a91f7076d0bd08a3b595ed9c7
Lovely.Value <aid.l.v.esd@gmail.com>
2023-11-05 14:04:27 +0300

    Lesson 37

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,52 @@
 const box = document.getElementById('box');
-
-console.log(box);
-
 const btns = document.getElementsByTagName('button');
-
-console.log(btns[0]);
-
 const circles = document.getElementsByClassName('circle');
-
-console.log(circles);
-
 const hearts = document.querySelectorAll('.heart');
+const oneHeart = document.querySelector('.heart');
+const wrapper = document.querySelector('.wrapper');
 
-console.log(hearts);
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+box.style.cssText = 'background-color: blue; width: 500px';
+btns[1].style.borderRadius = '100%';
+circles[1].style.backgroundColor = 'red';
+
+
+// for(let i = 0; i < hearts.length; i++) {
+//    hearts[i].style.backgroundColor = 'blue';
+// }
 
 hearts.forEach(item => {
-   console.log(item);
-});
+   item.style.backgroundColor = 'blue';
+})
 
-const oneHeart = document.querySelector('.heart');
+const div = document.createElement('div');
+// const text = document.createTextNode('I were here');
 
-console.log(oneHeart);
+div.classList.add('black');
+
+// document.body.append(div);
+
+// wrapper.append(div);
+// wrapper.appendChild(div);
+
+// wrapper.prepend(div);
+
+hearts[0].before(div);
+// hearts[0].after(div);
+
+// wrapper.insertBefore(div, hearts[1]);
+
+// circles[0].remove();
+
+// wrapper.removeChild(hearts[1]);
+
+// hearts[0].replaceWith(circles[1]);
+
+// wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = '<h1>Hello World</h1>';
+
+// div.textContent = 'Hello';
+
+div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');
