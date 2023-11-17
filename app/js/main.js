1d5ef25bf1cc1c433f1b0e7b40ba27d2dd330f05
Lovely.Value <aid.l.v.esd@gmail.com>
2023-11-17 13:53:13 +0300

    Lesson 44

--- app/js/main.js
+++ app/js/main.js
@@ -1,34 +1,12 @@
-//touchstart
-//touchmove
-//touchend
-//touchenter
-//touchleave
-//touchcancel
+const p = document.querySelectorAll('p');
+console.log(p);
 
-window.addEventListener('DOMContentLoaded', () => {
-   const box = document.querySelector('.box');
+function loadScript(src) {
+	const script = document.createElement('script');
+	script.src = src;
+	script.async = false;
+	document.body.append(script);
+}
 
-   box.addEventListener('touchstart', (e) => {
-      e.preventDefault();
-
-      console.log('start');
-      console.log(e.touches);
-      console.log(e.targetTouches);
-   });
-
-   box.addEventListener('touchmove', (e) => {
-      e.preventDefault();
-
-      console.log(e.targetTouches[0].pageX);
-   });
-
-   box.addEventListener('touchend', (e) => {
-      e.preventDefault();
-
-      console.log('end');
-   });
-});
-
-//touches
-//targetTouches
-//changedTouches
+loadScript('js/test.js');
+loadScript('js/some.js');
